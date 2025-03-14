6f7aadd61d8f35b6b7de131e65fbe04f5e823066
Adane Moges <adanemoges6@gmail.com>
2025-03-14 17:57:05 +0300

    Change server port from 8000 to 5001 in main.py

--- main.py
+++ main.py
@@ -303,4 +303,4 @@ async def schedule_email_report(request: ScheduleReportRequest):
 
 # Run the application
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=5001, reload=True)
